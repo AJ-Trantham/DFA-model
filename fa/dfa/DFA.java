6d949d656fee6f931d804a31dacfb76ca52bd133
AJ Trantham <ajtrantham@u.boisestate.edu>
2021-02-14 13:20:06 -0700

    stubbing out methods

---
+++ fa/dfa/DFA.java
@@ -0,0 +1,60 @@
+package fa.dfa;
+
+import fa.FAInterface;
+import fa.State;
+
+import java.util.Set;
+
+public class DFA implements DFAInterface, FAInterface {
+
+
+    @Override
+    public boolean accepts(String s) {
+        return false;
+    }
+
+    @Override
+    public State getToState(DFAState from, char onSymb) {
+        return null;
+    }
+
+    @Override
+    public void addStartState(String name) {
+
+    }
+
+    @Override
+    public void addState(String name) {
+
+    }
+
+    @Override
+    public void addFinalState(String name) {
+
+    }
+
+    @Override
+    public void addTransition(String fromState, char onSymb, String toState) {
+
+    }
+
+    @Override
+    public Set<? extends State> getStates() {
+        return null;
+    }
+
+    @Override
+    public Set<? extends State> getFinalStates() {
+        return null;
+    }
+
+    @Override
+    public State getStartState() {
+        return null;
+    }
+
+    @Override
+    public Set<Character> getABC() {
+        return null;
+    }
+}
