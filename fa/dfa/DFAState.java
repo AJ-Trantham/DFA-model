023378305725cb3d040a957b8f23bf169ef56eed
YeOldeGent <42984174+YeOldeGent@users.noreply.github.com>
2021-02-20 13:28:50 -0700

    Implemented DFAState
    Added everything that I believe needs to be in DFAState class

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,8 +1,23 @@
 package fa.dfa;
 
 import fa.State;
+import fa.TransitionMap;
 
 public class DFAState extends State {
 
         // should have a transition table which will map its transition on its possible inputs
+    private TransitionMap table; //table used to store transitions
+
+    public DFAState(){
+        table = new TransitionMap();
+    }
+
+    /**
+     * Returns the state the symbol would transition to
+     * @param symbol - next symbol in input string
+     * @return - new state, null if it has not been added
+     */
+    public State transition(String symbol){
+        return table.getTran(symbol);
+    }
 }
